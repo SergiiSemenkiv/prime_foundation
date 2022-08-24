f091fc0f591c451bb7c19242f2977beb4a8bac35
sergii-semenkiv <sergii.semenkiv@justeattakeaway.com>
2022-08-24 15:37:05 +0300

    Html markup ready

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,17 +1,42 @@
-import $ from 'jquery';
-import 'what-input';
+import $ from "jquery";
 
-// Foundation JS relies on a global variable. In ES6, all imports are hoisted
-// to the top of the file so if we used `import` to import Foundation,
-// it would execute earlier than we have assigned the global variable.
-// This is why we have to use CommonJS require() here since it doesn't
-// have the hoisting behavior.
-window.jQuery = $;
-require('foundation-sites');
+$(document).ready(function () {
+  const HEADER_SELECTOR = '.site-header'
+  const HEADER_STICKY_CLASS = 'site-header-sticky'
+  const MOBILE_MENU_SELECTOR = '#site__menu-mobile'
+  const MOBILE_MENU_LIST_SELECTOR = '.site-header__menu-mobile__list'
+  const MOBILE_MENU_TOGGLER_SELECTOR = '.site-header__menu-mobile__toggler'
+  const MOBILE_MENU_OPEN_CLASS = 'menu--open'
+  const NO_SCROLL_CLASS = 'no-scroll'
 
-// If you want to pick and choose which modules to include, comment out the above and uncomment
-// the line below
-//import './lib/foundation-explicit-pieces';
+  // mobile menu
+  $(MOBILE_MENU_TOGGLER_SELECTOR).on("click", function () {
+    const headerHeight = $(HEADER_SELECTOR).height()
+    $('html').toggleClass(NO_SCROLL_CLASS)
+    $(MOBILE_MENU_SELECTOR)
+      .toggleClass(MOBILE_MENU_OPEN_CLASS)
+      .css({top: headerHeight})
+  });
+  $(MOBILE_MENU_SELECTOR).on('click', function() {
+    $('html').toggleClass(NO_SCROLL_CLASS)
+    $(MOBILE_MENU_SELECTOR).toggleClass(MOBILE_MENU_OPEN_CLASS).css({top: headerHeight})
+  })
+  $(MOBILE_MENU_LIST_SELECTOR).click(function(event){
+    event.stopPropagation();
+  });
+  // sticky heading
 
+  function checkHeaderSticky() {
+    const header = $(HEADER_SELECTOR);
+    const headerHeight = $(HEADER_SELECTOR).height();
+    const scroll = $(window).scrollTop();
 
-$(document).foundation();
+    if (scroll >= headerHeight) header.addClass(HEADER_STICKY_CLASS);
+    else header.removeClass(HEADER_STICKY_CLASS)
+  }
+  checkHeaderSticky()
+  $(window).scroll(function(){
+    checkHeaderSticky()
+  });
+
+});
